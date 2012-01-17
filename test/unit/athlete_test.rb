87c4c9d2eca8726b0efffd969f3f1c0f39684d16
Seung Koh <skoh@uw.edu>
2012-01-16 22:03:14 -0800

    Added Test::Unit test cases for Athlete model class.

--- test/unit/athlete_test.rb
+++ test/unit/athlete_test.rb
@@ -4,4 +4,19 @@ class AthleteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "name is required" do
+    athlete = Athlete.new(:name => '', :sport => 'Football', :position => 'Running Back', :number => 24)
+    assert !athlete.valid?
+  end
+  
+  test "number must be integer" do
+    athlete = Athlete.new(:name => 'Marshawn Lynch', :sport => 'Football', :position => 'Running Back', :number => 24.5)
+    assert !athlete.valid?
+  end
+  
+  test "valid case" do
+    athlete = Athlete.new(:name => 'Marshawn Lynch', :sport => 'Football', :position => 'Running Back', :number => 24)
+    assert athlete.valid?
+  end  
 end
